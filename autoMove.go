1645a2d8aa0f63f392f167c0d852361a68746967
jayce <valoran.jayce@qq.com>
2022-02-24 17:48:40 +0800

    初始化

---
+++ autoMove.go
@@ -0,0 +1,30 @@
+package main
+
+// upx 压缩：upx -9 -o automove.exe .\go_build_autoMove_go.exe
+import (
+	"fmt"
+	"github.com/go-vgo/robotgo"
+	"math/rand"
+	"time"
+)
+
+func workTime() bool {
+	h := time.Now().Hour()
+	if 9 < h && h < 20 {
+		return true
+	}
+	return false
+}
+
+func main() {
+	w, h := robotgo.GetScreenSize()
+	fmt.Println("获取屏幕尺寸：", w, h)
+	for {
+		if workTime() {
+			x, y := rand.Intn(w), rand.Intn(h)
+			fmt.Println("移动到：", x, y)
+			robotgo.MoveMouseSmooth(x, y, 0.01, 0.02)
+		}
+		time.Sleep(time.Minute * 5)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module autoMove
+
+go 1.16
+
+require github.com/go-vgo/robotgo v0.100.2
